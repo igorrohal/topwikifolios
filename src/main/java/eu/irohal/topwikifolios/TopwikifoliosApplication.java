fc3c36159d11bed5ccc4efb48ec66412a4e63292
Igor Rohal <igor.rohal@hybris.com>
2017-11-05 20:55:22 +0100

    spring boot initializer commit

---
+++ src/main/java/eu/irohal/topwikifolios/TopwikifoliosApplication.java
@@ -0,0 +1,12 @@
+package eu.irohal.topwikifolios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopwikifoliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopwikifoliosApplication.class, args);
+	}
+}
