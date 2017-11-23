b43b6d8b8228555bbf40471a43958359961b8dbf
Igor Rohal <igor.rohal@hybris.com>
2017-11-23 18:38:37 +0100

    root endpoint returns text, /csv endpoint returns CSV

--- src/main/java/eu/irohal/topwikifolios/TopwikifoliosController.java
+++ src/main/java/eu/irohal/topwikifolios/TopwikifoliosController.java
@@ -3,14 +3,23 @@ package eu.irohal.topwikifolios;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.*;
+
 @RestController
 public class TopwikifoliosController {
 
     @Autowired
     private WikifolioParser wikifolioParser;
 
-    @RequestMapping(value = "/csv", produces = "text/csv")
-    public String csv() {
+    @RequestMapping(value = "/", produces = "text/plain; charset=UTF-8")
+    public String text(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        return wikifolioParser.getLatestCsvContents();
+    }
+
+    @RequestMapping(value = "/csv", produces = "text/csv; charset=UTF-8")
+    public String csv(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
         return wikifolioParser.getLatestCsvContents();
     }
 
